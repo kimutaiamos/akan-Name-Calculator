694f71c000ac9004a8772b6b76b31742ec40dcba
kimutaiamos <amos.kimutai@student.moringaschool.com>
2021-09-03 17:24:00 +0300

    added js functions

---
+++ script.js
@@ -0,0 +1,25 @@
+function getAkanName() {
+    let yearOfBirth = parseInt (document.getElementById("year-input").value);
+    let monthOfBirth = parseInt(document.getElementById("month-input").value);
+    let dayOfBirth = parseInt(document.getElementById("day-input").value);
+  
+    let genders = document.getElementsByName("gender");
+}
+
+    function dayValidator() {
+        if (monthOfBirth === 2 && Number(yearOfBirth) % 4 === 0) {
+          if (dayOfBirth > 28 || dayOfBirth < 1) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+            return false;
+          } else {
+            return true;
+          }
+        } else if (dayOfBirth < 1 || dayOfBirth > 31) {
+          return false;
+        } else {
+          return true;
+        }
+      }
